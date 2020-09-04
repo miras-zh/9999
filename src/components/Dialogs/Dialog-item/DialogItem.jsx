13889df0ff02631efd03fb7d5ee1f3ccf529ca5d
mikos8 <65290147+mikos8@users.noreply.github.com>
2020-09-05 00:31:46 +0600

    DialogItem export

--- src/components/Dialogs/Dialog-item/DialogItem.jsx
+++ src/components/Dialogs/Dialog-item/DialogItem.jsx
@@ -1,2 +1,15 @@
 import React from "react";
-import s from "./DialogItem.module.css";
+import s from "./../Dialogs.module.css";
+import { NavLink } from "react-router-dom";
+
+const DialogItem = (props) => {
+  return (
+    <div className={s.dialog}>
+      <NavLink to={"/dialogs/" + props.id} activeClassName={s.active}>
+        {props.name}
+      </NavLink>
+    </div>
+  );
+};
+
+export default DialogItem;
--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,17 +1,7 @@
 import React from "react";
 import s from "./Dialogs.module.css";
 import { NavLink } from "react-router-dom";
-
-const DialogItem = (props) => {
-  return (
-    <div className={s.dialog}>
-      <NavLink to={"/dialogs/" + props.id} activeClassName={s.active}>
-        {props.name}
-      </NavLink>
-    </div>
-  );
-};
-
+import DialogItem from "./Dialog-item/DialogItem";
 const Message = (props) => {
   return <div className={s.message}>{props.message}</div>;
 };
