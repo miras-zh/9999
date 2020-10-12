8bc5ba7565cc5545a90f02d5245955a82208a9e0
miraszh2020 <63465408+miraszh2020@users.noreply.github.com>
2020-10-12 08:09:30 +0600

    47

--- src/redux/message-reducer.js
+++ src/redux/message-reducer.js
@@ -20,17 +20,23 @@ let initialState = {
 
 const messageReducer = (state = initialState, action) => {
   switch (action.type) {
-    case ADD_MESSAGE:
+    case ADD_MESSAGE:{
+      
       let newMessage = {
         id: 5,
         messages: state.newMessageText,
       };
-      state.messageData.push(newMessage);
-      state.newMessageText = "";
-      return state;
-    case UPDATE_NEW_MESSAGE_TEXT:
-      state.newMessageText = action.newTextM;
-      return state;
+      let stateCopy = {...state};
+      stateCopy.messageData = [...state.messageData]
+      stateCopy.messageData.push(newMessage);
+      stateCopy.newMessageText = "";
+      return stateCopy; 
+    }
+    case UPDATE_NEW_MESSAGE_TEXT:{
+      let stateCopy = {...state}
+      stateCopy.newMessageText = action.newTextM;
+      return stateCopy;
+    }
     default:
       return state;
   }
